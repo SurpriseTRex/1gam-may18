0f617111712c97125bcd570bb07685d8d288082b
Sean Johnston <sean.c.johnston@gmail.com>
2018-05-01 06:56:22 +0100

    add slowly rotating player ship sprite and movement

--- main.lua
+++ main.lua
@@ -1,15 +1,19 @@
 local StarField = require 'stars/starfield'
+local Player = require 'player/player'
 
 function love.load()
     love.window.setMode(640, 360)
 
     starfield = StarField:new(500)
+    player = Player:new()
 end
 
 function love.update(dt)
     starfield:update(dt)
+    player:update(dt)
 end
 
 function love.draw()
     starfield:draw()
+    player:draw()
 end
---
+++ player/player.lua
@@ -0,0 +1,50 @@
+local class = require 'lib/middleclass'
+
+local Player = class('Player')
+
+function Player:initialize()
+    self.sprite = love.graphics.newImage("images/ship.png")
+    self.rotation = 0
+
+    self.x = 100
+    self.y = 160
+
+    self.speed = 15
+end
+
+function Player:update(dt)
+    self.rotation = self.rotation + (0.2 * dt)
+
+    local movementDir = getMovementInput()
+    print(movementDir.x .. "/" .. movementDir.y)
+    self.x = self.x + movementDir.x * dt * self.speed
+    self.y = self.y + movementDir.y * dt * self.speed
+end
+
+function Player:draw()
+    local offset_x = self.sprite:getWidth() / 2
+    local offset_y = self.sprite:getHeight() / 2
+
+    love.graphics.draw(self.sprite, self.x, self.y, self.rotation, 0.08, 0.08, offset_x, offset_y)
+end
+
+function getMovementInput()
+    local moveDir = {x = 0, y = 0}
+
+    if love.keyboard.isDown("w") then
+        moveDir.y = moveDir.y - 1
+    end
+    if love.keyboard.isDown("s") then
+        moveDir.y = moveDir.y + 1
+    end
+    if love.keyboard.isDown("a") then
+        moveDir.x = moveDir.x - 1
+    end
+    if love.keyboard.isDown("d") then
+        moveDir.x = moveDir.x + 1
+    end
+    
+    return moveDir
+end
+
+return Player
